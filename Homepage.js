f41b68b8f986793a85f70af3a8002b5a5d0c1176
Gabrielle Smith <74670296+smithgabs@users.noreply.github.com>
2021-03-08 13:15:04 -0500

    Update Homepage.js

--- Homepage.js
+++ Homepage.js
@@ -1,4 +1,5 @@
 import index.css from '/index.css'
+import index.html from '/index.html'
 let element = document.getElementById('favshow');
 element.onclick = function() { element.style.backgroundColor = 'blue' };
 
